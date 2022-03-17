0f223e774b1517a24b30d4fcc158a0fa5b43d948
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-03-17 08:52:17 -0400

    FB1.1 Business Model Created

---
+++ src/models/business.model.ts
@@ -0,0 +1,57 @@
+import { Rating } from './rating.model';
+
+interface IBusinessDetailed {
+    id: string;
+    hours: [number[]];
+    hasCoffee: boolean;
+    hasFood: boolean;
+    hasLine: boolean;
+    rating: number;
+}
+
+class Business {
+    constructor(
+        private id: string = ' ',
+        private hours: [number[]] = [[]],
+        private hasCoffee: boolean = false,
+        private hasFood: boolean = false,
+        private hasLine: boolean = false,
+        private ratings: Rating[] = [],
+    ) {}
+
+    public setRatting = (id: string, newRating: number): void => {
+        let exists: boolean = false;
+        for (let i = 0; i < this.ratings.length; i++) {
+            exists = this.ratings[i].getId() === id;
+            if (exists) {
+                this.ratings[i].updateRating(newRating);
+                break;
+            }
+        }
+        if (!exists) {
+            this.ratings.push(new Rating(id, newRating));
+        }
+    }
+
+    public getRating = () : number => {
+        const numberRatings: number = this.ratings.length;
+        const hasRatings: boolean = numberRatings > 0;
+        if (!hasRatings) {
+            return -1;
+        }
+        let filteredRatings: number[] = this.ratings.map((rate: Rating) => rate.getRating());
+        let totalRatings: number = filteredRatings.reduce((prev: number, curr: number) => prev + curr);
+        return totalRatings / numberRatings;
+    }
+
+    public getBusiness = () : IBusinessDetailed => {
+        return {
+            id: this.id,
+            hours: this.hours,
+            hasCoffee: this.hasCoffee,
+            hasFood: this.hasFood,
+            hasLine: this.hasLine,
+            rating: this.getRating()
+        } as IBusinessDetailed;
+    }
+}
