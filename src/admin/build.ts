e619bc673fe7f37fbedd19199d880353b44dd217
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-04-07 07:07:24 -0400

    Remove Failing Config Script

--- src/admin/build.ts
+++ src/admin/build.ts
@@ -1,18 +1,18 @@
-import { locations } from './config';
-import { store } from '../raven';
-import { Business } from '../models';
+// import { locations } from './config';
+// import { store } from '../raven';
+// import { Business } from '../models';
 
-(async() => {
-    console.log("Starting Upload...");
-    const bulkInsert = store.bulkInsert();
+// (async() => {
+//     console.log("Starting Upload...");
+//     const bulkInsert = store.bulkInsert();
 
-    for (let i = 0; i < locations.length; i++) {
-        let location = locations[i];
-        let business: Business = new Business(' ', location.name, location.coordinates, location.hours, location.hasCoffee, location.hasFood, location.hasLine, []);
-        await bulkInsert.store(business);
-        console.log(`Stored: ${location.name} | ${location.id}`);
-    }
+//     for (let i = 0; i < locations.length; i++) {
+//         let location = locations[i];
+//         let business: Business = new Business(' ', location.name, location.coordinates, location.hours, location.hasCoffee, location.hasFood, location.hasLine, []);
+//         await bulkInsert.store(business);
+//         console.log(`Stored: ${location.name} | ${location.id}`);
+//     }
 
-    await bulkInsert.finish();
-    console.log("Finished...");
-})();
+//     await bulkInsert.finish();
+//     console.log("Finished...");
+// })();
